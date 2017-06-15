c8ed29de51a26b7df591296183194e4923970e70
Edward Kim <mr.edward.kim@gmail.com>
2017-06-15 13:49:41 -0700

    Hook up maps

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
 import toMaterialStyle from 'material-color-hash';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const placeIcon = 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z';
 
@@ -13,34 +12,19 @@ const styles = StyleSheet.create({
 });
 
 class WrappedMap extends React.Component {
-
   constructor(props) {
     super(props);
 
-    console.log(this.props);
-
-    this.state = {position: {
-      latitude: this.props.userLocation.lat || 37.78825,
-      longitude: this.props.userLocation.lng || -122.4324,
-      latitudeDelta: 0.015,
-      longitudeDelta: 0.0121
-    }};
+    this.state = {
+      position: {
+        latitude: this.props.userLocation.lat || 37.78825,
+        longitude: this.props.userLocation.lng || -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121
+      }
+    };
 
     this.onSubmit = this.onSubmit.bind(this);
-    this.centerMoved = this.centerMoved.bind(this);
-  }
-
-  componentWillMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  componentDidUpdate(prevProps) {
-  }
-
-  centerMoved(region) {
-    //this.props.updateLotecation({lat: region.latitude, lng: region.longitude});
   }
 
   onSubmit(event) {
@@ -48,26 +32,25 @@ class WrappedMap extends React.Component {
   }
 
   render() {
-    const {lotecation, userLocation} = this.props;
+    const { lotecation, userLocation } = this.props;
     return (
-        <MapView provider="google"
-          style={styles.map}
-          region={this.state.position}
-          onRegionChange={this.centerMoved}
-          showsUserLocation={true}
-          loadingEnabled={true}
-          showsMyLocationButton={true}>
-          {this.props.lotes && this.props.lotes.map(lote => {
-            let color = toMaterialStyle(lote.loteSender.email);
-            return (
-              <MapView.Marker key={lote.id}
-              coordinate={{latitude: lote.location.latitude || 0,
-                longitude: lote.location.longitude || 0}}
-              pinColor={color.backGroundColor} />
-            );
-          })}
-        </MapView>
-
+      <MapView provider="google"
+        style={ styles.map }
+        region={ this.state.position }
+        onRegionChange={ this.centerMoved }
+        showsUserLocation={true}
+        loadingEnabled={true}
+        showsMyLocationButton={true}>
+        {this.props.lotes && this.props.lotes.map(lote => {
+          let color = toMaterialStyle(lote.loteSender.email);
+          return (
+            <MapView.Marker key={lote.id}
+            coordinate={{latitude: lote.location.latitude || 0,
+              longitude: lote.location.longitude || 0}}
+            pinColor={color.backGroundColor} />
+          );
+        })}
+      </MapView>
     );
   }
 }
